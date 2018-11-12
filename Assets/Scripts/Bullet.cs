1bff4102c11c5897f9c1b8d6e23aed26fd9d6ef2
Yovarni Yearwood <yovarniyearwood@gmail.com>
2018-11-11 22:33:11 -0500

    Initial Commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    public float speed = 30;
+
+    public Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        rigidBody.velocity = Vector2.up * speed;
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D (Collider2D col) {
+	    if (col.tag == "Wall")
+	    {
+
+	    }
+	}
+
+    
+}
